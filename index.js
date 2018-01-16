167a7514f1707ccbd28de2dff08cfd03bef49881
Jon Lazarini <jonathan.lazarini@wipro.com>
2018-01-16 23:02:02 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+class Symbol {
+  constructor(x, y, speed, isFirst, opacity) {
+    this._x = x
+    this._y = y
+    this._speed = speed;
+    this._isFirst = isFirst;
+    this._opacity = opacity;
+    this._switchInterval = 2
+  }
+
+  setRandomSymbol() {
+    return Math.random()
+  }
+}
+
+const s = new Symbol(1, 2, 3, false, .5)
+const d = s.setRandomSymbol()
