a1625aa48e380c382b4d8432ddb0524a320443b3
Jon Lazarini <jonathan.lazarini@wipro.com>
2018-01-17 19:20:25 +0000

    add character

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-/** */
+/** globals */
+let symbol = window.symbol;
+window.symbolSize = 48;
+
+/** canvas definition */
 class Symbol {
   constructor(x, y, speed, isFirst, opacity) {
     this._x = x
     this._y = y
-    this._speed = speed;
-    this._isFirst = isFirst;
-    this._opacity = opacity;
+    this._speed = speed
+    this._isFirst = isFirst
+    this._opacity = opacity
     this._switchInterval = 2
+    this.value;
   }
-
+  
   setRandomSymbol() {
-    return Math.random()
+    // Katakana from 0 - 96 unicode chars - U+30x
+    return this.value = String.fromCharCode(0x30A0 + Math.round(0, 96))
+  }
+  
+  render() {
+    fill(0, 255, 70)
+    text(this.value, this._x, this._y)
   }
 }
 
-const s = new Symbol(1, 2, 3, false, .5)
-const d = s.setRandomSymbol()
 
+symbol = new Symbol(
+  window.innerWidth / 2,
+  window.innerHeight / 2
+)
 
 /**
  * p5
  */
 function setup() {
+  window.createCanvas(window.innerWidth, window.innerHeight)
+  background(0)
+  symbol.setRandomSymbol()
+  textSize(symbolSize)
 }
 
 function draw() {
-  background(0)
-  ellipse(50, 50, 80, 80);
+  symbol.render()
 }
